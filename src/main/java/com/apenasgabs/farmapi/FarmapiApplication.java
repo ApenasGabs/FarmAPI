02d34abe7da4fc7f4997a13071a1a2e276731096
Gabriel Rodrigues <gabers357@gmail.com>
2024-03-26 09:54:18 -0300

    Criado arquivos inciais

---
+++ src/main/java/com/apenasgabs/farmapi/FarmapiApplication.java
@@ -0,0 +1,13 @@
+package com.apenasgabs.farmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apenasgabs/farmapi/FarmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apenasgabs.farmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
