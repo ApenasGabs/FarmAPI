19b6dd7450da16b69f000573d9ed047b27c16303
Gabriel Rodrigues <gabers357@gmail.com>
2024-03-26 12:30:42 -0300

    Adicionado metodo findByNomeContainingIgnoreCase no ProdutoRepository

--- src/main/java/com/apenasgabs/farmapi/repository/ProdutoRepository.java
+++ src/main/java/com/apenasgabs/farmapi/repository/ProdutoRepository.java
@@ -1,8 +1,13 @@
 package com.apenasgabs.farmapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.apenasgabs.farmapi.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+  List<Produto> findByNomeContainingIgnoreCase(String nome);
+
 }
